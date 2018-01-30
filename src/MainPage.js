8019b9f196b8cb6ad727dbfa0f54d3ab49a14e5f
Javier Abad <javierabad@Javiers-MacBook-Pro.local>
2018-01-30 23:05:48 +0100

    Steps

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import ModalDialog from './ModalDialog';
 
 export default class MainPage extends Component {
+  hello(hsCode) {
+  	console.log("salvando", hsCode);
+  }
+
   render() {
     return (
       <div>
-        <ModalDialog />
+        <ModalDialog onChange={this.hello}/>
       </div>
     )
   }
--- src/ModalDialog.js
+++ src/ModalDialog.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import WizardStep from 'react-wizard-step';
 import SelectChapter from './components/SelectChapter';
 import SelectHeading from './components/SelectHeading';
 import SelectSubheading from './components/SelectSubheading';
@@ -10,12 +9,12 @@ export default class ModalDialog extends Component {
   
   constructor(props) {
     super(props);
-    console.log("cion");
     this.state = {
       modal: false,
       chapter: null,
       heading: null,
-      subheading: null
+      subheading: null,
+      step: 0
     };
 
     this.toggle = this.toggle.bind(this);
@@ -23,6 +22,7 @@ export default class ModalDialog extends Component {
     this.updateHeading = this.updateHeading.bind(this);
     this.updateSubheading = this.updateSubheading.bind(this);
     this.updateChapter = this.updateChapter.bind(this);
+    this.setStep = this.setStep.bind(this);
   }
 
   toggle() {
@@ -63,8 +63,43 @@ export default class ModalDialog extends Component {
     if(this.state.subheading) {
       text += " - " + this.state.subheading;
     }
-    console.log("titulo", text);
     return text;
+  }
+
+  setStep(step) {
+    this.setState({step});    
+  }
+
+  printStep() {
+    if (this.state.step === 0) {      
+      return (
+        <div>
+          <SelectChapter propertyHandler={this.updateChapter}/>
+          <Button color="primary" onClick={() => this.setStep(1)}>Next</Button>
+        </div>
+      )
+    }
+    if (this.state.step === 1) {      
+      return (
+        <div>
+          <SelectHeading propertyHandler={this.updateHeading}/>
+          <Button color="primary" onClick={() => this.setStep(0)}>Back</Button>
+          <Button color="primary" onClick={() => this.setStep(2)}>Next</Button>
+        </div> 
+      )
+    }
+    if (this.state.step === 2) {      
+      return (
+        <div>
+          <SelectSubheading propertyHandler={this.updateSubheading}/>
+          <Button color="primary" onClick={() => this.setStep(1)}>Back</Button>
+        </div> 
+      )
+    }
+  }
+
+  buttonEnabled() {
+    return this.state.subheading === null;
   }
 
   render() {    
@@ -74,15 +109,11 @@ export default class ModalDialog extends Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} className=''>
           <ModalHeader toggle={this.toggle}>{this.getTitle()}</ModalHeader>
           <ModalBody>
-            <WizardStep isShowStepBar={false}>
-              <SelectChapter propertyHandler={this.updateChapter}/>
-              <SelectHeading propertyHandler={this.updateHeading}/>
-              <SelectSubheading propertyHandler={this.updateSubheading}/>
-            </WizardStep>
+            {this.printStep()}
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button color="primary" onClick={() => this.props.onChange(this.getTitle())} disabled={this.buttonEnabled()}>Save</Button>            
           </ModalFooter>
         </Modal>
       </div>
